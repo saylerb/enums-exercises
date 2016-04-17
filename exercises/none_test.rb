d49feceb5bce326556d343f8c38b3fbeca534642
saylerb <sayler.b@gmail.com>
2016-04-16 18:25:31 -0600

    complete none test

--- exercises/none_test.rb
+++ exercises/none_test.rb
@@ -1,4 +1,4 @@
-gem 'minitest'
+require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -15,36 +15,32 @@ class NoneTest < Minitest::Test
   def test_double_negative
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = numbers.none? do |number|
-      # Your code goes here
+      number < 0
     end
     refute not_none_negative
   end
 
   def test_none_are_negative
-    skip
     numbers = [9, 3, 1, 8, 3, 3, 5]
-    # Your code goes here
+    none_negative = numbers.none? { |num| num < 0 }
     assert none_negative
   end
 
   def test_none_shall_pass
-    skip
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
-    # Your code goes here
+    none_shall_pass = critters.none? { |critter| critter == "shall pass" }
     assert none_shall_pass
   end
 
   def test_one_or_more_shall_pass
-    skip
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    none_shall_pass = phrases.none? { |phrase| phrase == "shall pass" }
     refute none_shall_pass
   end
 
   def test_none_even
-    skip
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = numbers.none? { |num| num.even? }
     assert none_even
   end
 
