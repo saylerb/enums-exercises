0370dfeb7bae538cafbdde796f28a63f808363b6
saylerb <sayler.b@gmail.com>
2016-04-23 08:42:02 -0600

    start zip test

--- exercises/basic_enums_test.rb
+++ exercises/basic_enums_test.rb
@@ -6,16 +6,15 @@ class BasicEnumsTest < Minitest::Test
   def test_solve_for_x
     s = ''
     5.times do
-      # write code here
+      s.concat('x')
     end
     assert_equal 'xxxxx', s
   end
 
   def test_simple_sum
-    skip
     sum = 0
     numbers = [1, 2, 3, 4, 5]
-    # write code here
+    sum = numbers.reduce(:+)
     assert_equal 15, sum
   end
 
